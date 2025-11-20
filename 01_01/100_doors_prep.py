ac1ec90ca6b8d95099e4947e5d76993a3affa51f
Taph-tech <mustaphac687@gmail.com>
2025-11-20 16:36:18 -0500

    Indentation corrected after pasting

--- 01_01/100_doors_prep.py
+++ 01_01/100_doors_prep.py
@@ -28,40 +28,36 @@ for x in range(3, 101, 3):
 
 print('4th pass:'* 10)
 for x in range(4, 101, 4):
-doors[i] = not doors[i]
-count +=1
-print("Toggling door", doors[i] ,count)
+   doors[i] = not doors[i]
+   count +=1
+   print("Toggling door", doors[i] ,count)
 
 print('5th pass:'* 10)
 for x in range(5, 101, 5):
-doors[i] = not doors[i]
-count +=1
-print("Toggling door", doors[i] ,count)
+  doors[i] = not doors[i]
+  count +=1
+  print("Toggling door", doors[i] ,count)
 
 print('6th pass:'* 10)
 for x in range(6, 101, 6):
-doors[i] = not doors[i]
-count +=1
-print("Toggling door", doors[i] ,count)
+   doors[i] = not doors[i]
+   count +=1
+   print("Toggling door", doors[i] ,count)
 
 print('7th pass:'* 10)
 for x in range(7, 101, 7):
-doors[i] = not doors[i]
-count +=1
-print("Toggling door", doors[i] ,count)
+  doors[i] = not doors[i]
+  count +=1
+  print("Toggling door", doors[i] ,count)
 
 print('8th pass:'* 10)
 for x in range(8, 101, 8):
-doors[i] = not doors[i]
-count +=1
-print("Toggling door", doors[i] ,count)
+  doors[i] = not doors[i]
+  count +=1
+  print("Toggling door", doors[i] ,count)
 
 print('9th pass:'* 10)
 for x in range(9, 101, 9):
-doors[i] = not doors[i]
-count +=1
-print("Toggling door", doors[i] ,count)
-# Detour - steps in for loop
-'''for i in range(1, 11, 2):
-print(i)
-'''
+  doors[i] = not doors[i]
+  count +=1
+  print("Toggling door", doors[i] ,count)
