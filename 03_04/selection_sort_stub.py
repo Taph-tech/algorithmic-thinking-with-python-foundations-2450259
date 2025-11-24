c981fce2a34b7c55d2f4d710dd78968a02272d7f
Taph-tech <mustaphac687@gmail.com>
2025-11-24 14:05:02 -0500

    Opened the file selection_sort_stub

--- 03_04/selection_sort_stub.py
+++ 03_04/selection_sort_stub.py
@@ -1,8 +1,15 @@
 def selection_sort(xs):
     pass
+    for i in range(len(xs)-1):
+        min_index = i
+        for j in range(i + 1, len(xs)):
+          if xs[j] < xs[min_index]:
+           min_index = j
+        xs[i], xs[min_index] = xs[min_index], xs[i]
 
 
-xs = [3, 2, 1, 5, 4]
+#xs = [3, 2, 1, 5, 4]
+xs = [64, 25, 12, 22, 11]
 print(xs)
 selection_sort(xs)
 print(xs)
